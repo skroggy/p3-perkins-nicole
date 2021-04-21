5d2dd7bc474c91a2ddb118885240980252b5aba7
Nicole Perkins <perkinsn@ufl.edu>
2021-04-21 02:46:03 -0500

    coded generator

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { header } from './modules/header';
 import { footer } from './modules/footer';
-import { burger } from './modules/lunch-generator';
+// import { burger } from './modules/lunch-generator';
 
 class App {
     constructor() {
@@ -23,34 +23,43 @@ class App {
         font-style: normal;
         color: white;
         font-size: 16pt;
+        text-align: center;
         background-image: url("http://dearnoire.com/p3/bg.png");
-        background-position: 58.5% center;
+        background-position: 58.5%;
         background-repeat: no-repeat;
         background-attachment: fixed;
         background-size: cover;
     }
-    
-    
+
+    header {
+        padding: 2%;
+        background-color: #1149bb;
+    }
+
+    h1 {
+        display: none;
+    }
+
+    #title {
+        width: 65%;
+        max-width: 250px;
+    }
+
+    div {
+        text-align: center;
+        margin-top: 100px;
+        padding-bottom: 2%;
+    }
+
     button {
         background-color: #ed2130;
         padding: 1%;
         max-width: 250px;
         width: 70%;
         box-shadow: black -4px 6px;
-        position: fixed;
-        top: 50%;
-        left: 13%;
+        display: inline-block;
     }
-    
-    p {
-        width: 125px;
-        line-height: 25px;
-        position: fixed;
-        top: 28%;
-        right: 33%;
-        text-align: center;
-    }
-    
+
     button img {
         width: 100%;
     }
@@ -58,18 +67,66 @@ class App {
     button:hover {
         background-color: #f6e122;
     }
+
+    figure {
+        display: block;
+        padding-top: 4%;
+    }
+
+    #burger {
+        width: 15%;
+        min-width: 250px;
+        max-width: 350px;
+    }
+
+    footer {
+        display: none;
+    }
+
+    @media (min-width: 768px) {
+        div {
+            margin-top: 40px;
+        }
+    }
     </style>
 
-        <div>
         <h1>${header.title}</h1>
-        <button><img src="http://dearnoire.com/p3/order.png" alt=""></button>
-        <figure id="burger"></figure>
+
+        <header><img id="title" src="http://dearnoire.com/p3/title2.png"></header>
+
+        <div>
+        <figure><img id="burger" src=""></figure>
+        <button><img src="http://dearnoire.com/p3/order.png"></button>
         </div>
-        <p>${footer.foot}</p>
+
+        <footer>${footer.foot}</footer>
         `
         ;
 
         document.body.innerHTML = template;
+
+        const imageArray = [
+            "http://dearnoire.com/p3/warnpeas.png",
+            "http://dearnoire.com/p3/hamlet.png",
+            "http://dearnoire.com/p3/beetcar.png",
+            "http://dearnoire.com/p3/avocadontworry.png",
+            "http://dearnoire.com/p3/cornsupremecy.png",
+            "http://dearnoire.com/p3/friednpredjudice.png",
+            "http://dearnoire.com/p3/lanceshallot.png",
+            "http://dearnoire.com/p3/lanceshallot.png",
+            "http://dearnoire.com/p3/licensekale.png",
+            "http://dearnoire.com/p3/newyolk.png" ]
+        
+        const image = document.getElementById("burger");
+        const button = document.querySelector("button");
+        
+        button.addEventListener("click", () => generateRandomPicture(imageArray));
+        
+        function generateRandomPicture(array){
+            let randomNum = Math.floor(Math.random() * array.length); 
+            image.setAttribute("src", array[randomNum]);
+        }
+        
     }
 }
 
--- src/modules/lunch-generator.js
+++ src/modules/lunch-generator.js
@@ -1,7 +1,25 @@
 class Burger {
-    constructor(burger){
-        this.burger = burger;
+    constructor(){
+        this.randomBurger();
+    }
+
+    randomBurger() {
+        const imageArray = [
+            "http://dearnoire.com/p3/warnpeas.png",
+            "http://dearnoire.com/p3/hamlet.png",
+            "http://dearnoire.com/p3/beetcar.png",
+        ];
+        
+        const image = document.querySelector("img");
+        const button = document.querySelector("button");
+        
+        button.addEventListener("click", () => generateRandomPicture(imageArray));
+        
+        function generateRandomPicture(array){
+            let randomNum = Math.floor(Math.random() * array.length); 
+            image.setAttribute("src", array[randomNum]);
+        }
     }
 }
-
-export const burger = new  Burger('Burger Generator');
+   
+// export const burger = new  Burger();
